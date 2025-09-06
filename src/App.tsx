ae7d135c0162b30a58b81e17c2685efb5e01f19f
ChangHyeon <canghaun@naver.com>
2025-09-06 17:46:14 +0900

    :building_construction: Build: 프로젝트 빌드

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Chat from "./pages/Chat";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return <Chat />;
+}
+
+export default App;
